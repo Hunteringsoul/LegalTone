9645ff2fe83bd656ab52c21a90573436dba13cd4
Ketan Agarwal <ketanagarwal282004@gmail.com>
2026-05-28 15:43:18 +0530

    Add vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
+import { Analytics } from "@vercel/analytics/react"
+
+
+<StrictMode>
+  <ErrorBoundary>
+    <App />
+    <Analytics />
+  </ErrorBoundary>
+</StrictMode>
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
